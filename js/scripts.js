422692e90778b0381197ab634c37a7819ffa4a27
maranatha-ruth <maranatharuth6@gmail.com>
2019-01-18 18:01:10 +0200

    added button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,42 @@
-$(document).ready(function(){
-    $("form#idjambotron").submit
+// Business logic 
+function form(firstName){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.accountNo=accountNo;
+    this.date=date;
+    this.depositMoney=depositMoney;
+    this.balance=balance;
+    this.withdraw=withdraw;
 }
+Myfunction()
+function(container1)
+return container1
+
+var ruth=form#Container1.value()
+return;
+
+document.getElementById("container1").innerHTML
+
+
+ 
+
+//user interface logic(front-end)
+$(document).ready(function () {
+    $("form").submit(function (event) {
+        var contaniner1 = [];
+        $("#result").text("your score is " + getinformation(answers));
+        $("#story").show();
+        event.preventDefault();
+    });
+    });
+    $("#submit").click(function(){
+        $("#container1").toggle();
+        $("#result").slidetoggle();
+        $("#bank account").show();
+    });
+    // $("#score").click(function () {
+    //     $("#result").toggle();
+    //     $("#quiz").slidetoggle();
+    //     $("#score").toggle();
+    // });
+    
