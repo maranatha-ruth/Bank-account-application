fd370110b5cf9622286cfdc50b9f68e161d15392
maranatha-ruth <maranatharuth6@gmail.com>
2019-01-19 08:37:07 +0200

    added check buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,33 @@
-// Business logic 
-function form(firstName){
-    this.firstName=firstName;
-    this.lastName=lastName;
-    this.accountNo=accountNo;
-    this.date=date;
-    this.depositMoney=depositMoney;
-    this.balance=balance;
-    this.withdraw=withdraw;
-}
-Myfunction()
-function(container1)
-return container1
-
-var ruth=form#Container1.value()
-return;
-
-document.getElementById("container1").innerHTML
-
-
- 
-
-//user interface logic(front-end)
-$(document).ready(function () {
-    $("form").submit(function (event) {
-        var contaniner1 = [];
-        $("#result").text("your score is " + getinformation(answers));
-        $("#story").show();
-        event.preventDefault();
-    });
-    });
-    $("#submit").click(function(){
-        $("#container1").toggle();
-        $("#result").slidetoggle();
-        $("#bank account").show();
-    });
-    // $("#score").click(function () {
-    //     $("#result").toggle();
-    //     $("#quiz").slidetoggle();
-    //     $("#score").toggle();
-    // });
-    
+var BankAccount = {
+    balance: 0,
+    withdraw: function(amount) {
+        this.balance = this.balance - amount;
+    },
+    deposit: function(amount) {
+        this.balance = this.balance + amount;
+    }
+  };
+  $(document).ready(function() {
+  var newAccount = Object.create(BankAccount);
+  $("form#create").submit(function(event) {
+    event.preventDefault();
+    var inputtedName = $("input#name").val();
+    var inputtedBalance = parseInt($("input#initial").val());
+    newAccount.name = inputtedName;
+    newAccount.deposit(inputtedBalance);
+    $(".balance").text(newAccount.balance);
+  });
+  $("form#manage").submit(function(event) {
+    event.preventDefault();
+    var newBalance = parseInt($("input#deposit").val());
+    newAccount.deposit(newBalance);
+    $(".balance").text(newAccount.balance);
+  // $("#manage").unbind('submit');
+  });
+  $("form#withdraw").submit(function(event) {
+    event.preventDefault();
+    var newBalance = parseInt($("input#withdraw").val());
+    newAccount.withdraw(newBalance);
+    $(".balance").text(newAccount.balance);
+  });
+  });
