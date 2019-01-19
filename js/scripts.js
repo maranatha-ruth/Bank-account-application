a1ad69787f4b3d333afda98a8b3011576296694d
maranatha-ruth <maranatharuth6@gmail.com>
2019-01-19 10:49:38 +0200

    added link

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 var BankAccount = {
   balance: 0,
-  withdraw: function(amount) {
-      this.balance = this.balance - amount;
-  },
   deposit: function(amount) {
       this.balance = this.balance + amount;
+  },
+  withdraw: function(amount) {
+      this.balance = this.balance - amount;
   }
 };
 $(document).ready(function() {
-var newAccount = Object.create(BankAccount);
-$("form#create").submit(function(event) {
-  event.preventDefault();
-  var inputtedName = $("input#name").val();
-  var inputtedBalance = parseInt($("input#initial").val());
-  newAccount.name = inputtedName;
-  newAccount.deposit(inputtedBalance);
-  $(".balance").text(newAccount.balance);
-});
+  var newAccount = Object.create(BankAccount);
+  $("form#create").submit(function(event) {
+    event.preventDefault();
+    var inputtedName = $("input#name").val();
+    var inputtedBalance = parseInt($("input#initial").val());
+    newAccount.name = inputtedName;
+    newAccount.deposit(inputtedBalance);
+    $(".balance").text(newAccount.balance);
+  });
 $("form#manage").submit(function(event) {
   event.preventDefault();
   var newBalance = parseInt($("input#deposit").val());
   newAccount.deposit(newBalance);
   $(".balance").text(newAccount.balance);
-// $("#manage").unbind('submit');
+
 });
 $("form#withdraw").submit(function(event) {
   event.preventDefault();
